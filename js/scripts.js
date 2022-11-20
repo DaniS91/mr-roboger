affd00e102509d541077dd6eef1bb3ca0b801184
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2022-11-19 16:31:53 -0800

    restructure js function to replace any digit containing 3 with a string: second TDD test passed !

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,17 @@
 //Utility Logic
 
 //Business Logic
-function countUpTo (number) {
+function countAndReplace (number) {
 let numArray = [];
 for (let i = 0; i <= number; i+=1) {
-  numArray.push(i);
+  numArray.push(i.toString());
 }
-return numArray;
-}
-
-function replaceNumThatContains(numArray) {
-  const newArray = [];
-  numArray.forEach(function(element) {
-    if (element === 3) {
-      newArray.push("Won't you be my neighbor?");
-    } else {
-      newArray.push(element);
-    }
-  });
-  return newArray;
+const newArray = numArray.map(function(element) {
+  if (element.includes("3")) {
+    return "Won't you be my neighbor?";
+  } else {
+    return element;
+  }
+});
+return newArray;
 }
